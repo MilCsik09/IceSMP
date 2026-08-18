b2edccd60b8c8519df8e246f399cb7a03bfe2986
Csik Milán <81396861+MilCsik09@users.noreply.github.com>
2026-08-18 22:24:04 +0200

    test(equipment): launch opt-in Paper runtime probe

--- src/main/java/hu/taliann/icesmp/IceSMP.java
+++ src/main/java/hu/taliann/icesmp/IceSMP.java
@@ -36,6 +36,7 @@ public final class IceSMP extends JavaPlugin {
         }
 
         resourcePackListener.resendCurrent();
+        hu.taliann.icesmp.itemization.PaperSourceIntegrityRuntimeProbe.maybeRun(this, core);
     }
 
     @Override
